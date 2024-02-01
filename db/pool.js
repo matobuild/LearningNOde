ce90dfc9dcd4f8d1e6d8a59b86b48e802c2f95dd
Mato <Kittawat@icloud.com>
2024-02-01 14:18:36 +0800

    55

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg")
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config.env" })
 
 const pool = new Pool({
-  user: "hnlenrem",
-  host: "arjuna.db.elephantsql.com",
-  database: "hnlenrem",
-  password: "gakqmr5PxEprmVglSkCylD6a2iE4yztK",
-  port: 5432,
+  // user: "hnlenrem",
+  // host: "arjuna.db.elephantsql.com",
+  // database: "hnlenrem",
+  // password: "gakqmr5PxEprmVglSkCylD6a2iE4yztK",
+  // port: 5432,
+
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 module.exports = pool
--- service/userService.js
+++ service/userService.js
@@ -1,23 +1,85 @@
-exports.getAllUsers = (req, res) => {
-  res.status(200).json({ status: "success", data: "Function test not defined" })
+const pool = require("../db/pool")
+
+exports.getAllUsers = async (req, res) => {
+  let sql = "SELECT * FROM public.customers"
+  let response = await pool.query(sql)
+  console.log(response)
+  if (response.rowCount > 0) {
+    res.status(200).json({ status: "success", data: response.rows })
+  } else {
+    res.status(400).json({ status: "fail", data: "data not found" })
+  }
 }
 
-exports.createUser = (req, res) => {
-  res.status(200).json({ status: "success", data: "Function test not defined" })
+exports.createUser = async (req, res) => {
+  let body = req.body
+  console.log("BODY", body)
+  let sql = `INSERT INTO public.customers
+( firstname, lastname, email, city, job)
+VALUES( '${body.firstname}',  '${body.lastname}',  '${body.email}',  '${body.city}', '${body.job}');`
+  console.log("SQL", sql)
+  let response = await pool.query(sql)
+  console.log(response)
+  if (response.rowCount > 0) {
+    res.status(200).json({ status: "success", data: "Insert data success" })
+  } else {
+    res.status(400).json({ status: "fail", data: "Insert data fail" })
+  }
 }
 
-exports.getUserById = (req, res) => {
-  res.status(200).json({ status: "success", data: "Function test not defined" })
+exports.getUserById = async (req, res) => {
+  let { id } = req.params
+  let sql = "select * from public.customers where customerID = $1"
+  let response = await pool.query(sql, [id])
+  console.log(response)
+
+  if (response.rowCount > 0) {
+    res.status(200).json({ status: "success", data: response.rows[0] })
+  } else {
+    res.status(400).json({ status: "fail", data: "data not found" })
+  }
 }
 
-exports.updateUserById = (req, res) => {
-  res.status(200).json({ status: "success", data: "Function test not defined" })
+exports.updateUserById = async (req, res) => {
+  let { id } = req.params
+  let body = req.body
+  let sql = `UPDATE Customers
+  SET firstName = $1, lastName = $2, email = $3, city = $4, job= $5
+  WHERE CustomerID = $6;`
+
+  let response = await pool.query(sql, [
+    body.firstname,
+    body.lastname,
+    body.email,
+    body.city,
+    body.job,
+    id,
+  ])
+  console.log(response)
+
+  if (response.rowCount > 0) {
+    res.status(200).json({ status: "success", data: "update data success" })
+  } else {
+    res.status(400).json({ status: "fail", data: "update data fail" })
+  }
 }
 
-exports.deleteUserById = (req, res) => {
-  res.status(200).json({ status: "success", data: "Function test not defined" })
+exports.deleteUserById = async (req, res) => {
+  let { id } = req.params
+
+  let sql = `DELETE FROM Customers WHERE CustomerID = $1;`
+
+  let response = await pool.query(sql, [id])
+  console.log(response)
+
+  if (response.rowCount > 0) {
+    res.status(200).json({ status: "success", data: "delete data success" })
+  } else {
+    res.status(400).json({ status: "fail", data: "delete data fail" })
+  }
 }
 
+// --------------------------------
 exports.checkID = (req, res, next, val) => {
   if (Number(val) <= 0) {
     res.status(400).json({
