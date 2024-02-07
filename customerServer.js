a909a63a822d0a1b2ba9c58779c5410e8cbf3740
Mato <Kittawat@icloud.com>
2024-02-07 15:49:07 +0800

    security security HTTP header

--- customerServer.js
+++ customerServer.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 const router = require("./router/router")
 const errors = require("./utils/errors")
 const rateLimit = require("express-rate-limit")
-
+const helmet = require("helmet")
 const app = express()
 
 dotenv.config({ path: "./config.env" })
@@ -12,7 +12,8 @@ const port = process.env.PORT || 8080
 
 console.log(process.env.USERNAME)
 
-app.use(express.json())
+app.use(helmet())
+app.use(express.json({ limit: "100kb" }))
 app.use(morgan("dev"))
 // get static file from public folder
 app.use(express.static("./public"))
